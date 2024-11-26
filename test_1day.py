d3c34a95171c39a86309dc23ffa5dfc62d797af4
saber0069 <gaotianyu94945@gmail.com>
2024-11-26 23:32:07 +0800

    Add files via upload

---
+++ test_1day.py
@@ -0,0 +1,11 @@
+a = ("1231","啊啊啊啊","1assd")
+print(type(a),a)
+#圆括号是元组
+b = ["1231","啊啊啊啊","1assd"]
+print(type(b),b)
+
+c = "123112312112"
+print(type(c))
+
+d = {"1":1,"2":2}
+print(type(d),d["1"])
