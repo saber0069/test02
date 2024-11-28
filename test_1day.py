f465f05e20b00deed067805bc4bc0e1f8fa0e580
saber0069 <gaotianyu94945@gmail.com>
2024-11-28 23:40:37 +0800

    Add files via upload

--- test_1day.py
+++ test_1day.py
@@ -1,11 +1,70 @@
-a = ("1231","啊啊啊啊","1assd")
-print(type(a),a)
-#圆括号是元组
-b = ["1231","啊啊啊啊","1assd"]
-print(type(b),b)
+# a = ("1231","啊啊啊啊","1assd")
+# print(type(a),a)
+# #圆括号是元组
+# b = ["1231","啊啊啊啊","1assd"]
+# print(type(b),b)
+#
+# c = "123112312112"
+# print(type(c))
+#
+# d = {"1":1,"2":2}
+# print(type(d),d["1"])
 
-c = "123112312112"
-print(type(c))
+# name = "pengyuyyan"
+# age = 40
+# height = 182.0
+# print(name)
+# print(age)
+# print(height)
 
-d = {"1":1,"2":2}
-print(type(d),d["1"])
+#给变量largest_city赋值字符串"Beijing"，并打印变量largest_city
+# largest_city = "Beijing"
+# print(largest_city)
+
+# 给变量city赋值"Beijing"，然后打印变量city，再给变量city赋值"Shanghai"，然后打印变量city
+# city = "Beijing"
+# print(city)
+# city = "Shanghai"
+# print(city)
+
+# 我们写两行代码，第一行：用代码来计算1+1，并赋值给变量answer；第二行：打印变量answer
+# answer = 1+1
+# print(answer)
+
+# 我们同样可以只写一行代码，来完成上面的功能，直接打印1 + 1
+# print(1+1)
+
+# 第一行代码，计算8 - 2，并赋值给answer1，第二行代码打印变量answer1
+# 第三行代码，计算8/ 2，并赋值给answer2，第四行代码打印变量answer2
+# answer1 = 8-2
+# print(answer1)
+# answer2 = 8/2
+# print(answer2)
+
+# 第一行代码，计算1.5 + 1.5，直接打印结果
+# 第二行代码，计算1.5 * 1.5，直接打印结果
+# print(1.5+1.5)
+# print(1.5*1.5)
+
+# 计算双十一购买的商品需要付多少钱
+# 单个商品价格分别保存在变量price1，price2，price3中，折扣存储在变量discount中
+# 1.将单个商品价格相加计算出商品总价
+# 2.将商品总价*折扣计算出应付总金额，并将应付总金额赋值给变量total
+# 2.使用print输出结果tota
+# price1 = 286
+# price2 = 399
+# price3 = 899
+# discount = 0.7
+# total = (price1+price2+price3)*discount
+# print(total)
+
+# 用取整数商//的方法，计算111在百位上的数值，并输出。
+# print(111//100)
+
+# 计算520除以2的余数，将结果赋值给变量result，使用print()输出result
+# result = 520%2
+# print(result)
+
+# 给变量account赋值5000，然后使用格式化输出打印print(f"您的账户余额{account}")
+# account = 5000
+# print(f"您的账户余额{account}")
